75a3b9864472aed1f4861677037916928fbf4270
Online Yazılı Hazırlama <biserkanla@gmail.com>
2026-04-20 22:30:11 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import A4Template from "@/components/A4Template";
 function App() {
   return (
     <main className="min-h-screen w-full flex flex-col">
- 	<A4Template />
+      <MobileWarning />
+     	<A4Template />
     </main>
   );
 }
